95501eeebc7d557be4cef1c20e405309e060a34a
Jonn Nilsson <jonn@jonn.nu>
2021-05-30 09:25:04 +0200

    fix: remove unused exports

--- src/class/index.ts
+++ src/class/index.ts
@@ -1,2 +1,1 @@
-export * from "./KeyPairHandler";
 export * from "./Keystore";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./class";
 export * from "./entity";
 export * from "./enum";
-export * from "./error";
 export * from "./util";
